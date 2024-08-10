ce8116f5e7f4fcd8543349710dfd6c9356a11359
Aaron Rodrigues <contact@aaronrodrigues.com>
2024-08-10 22:49:52 +0200

    first commit

---
+++ program.py
@@ -0,0 +1,7 @@
+def main():
+    name = input("Tenant name: ")
+    plot = input("Plot number: ")
+    number = input("Tenant phone number:")
+    rent = input("Rent per month")
+    startdate = input("Start date")
+    
