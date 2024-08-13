9360b2409143132322c4cb04beb715fd5fa7bf2b
Aaron Rodrigues <contact@aaronrodrigues.com>
2024-08-13 21:36:40 +0200

    continue program

--- program.py
+++ program.py
@@ -2,33 +2,51 @@ import PySimpleGUI as gui
 import sqlite3
 
 def main():
+    layout = [ [gui.Text("What would you like to do? ")],
+               [gui.Button("Add tenant")],
+               [gui.Button("Check due dates")],
+               [gui.Button("Remove tenant")]
+               ]
+    
+    window = gui.Window("Choose an option", layout)
+
+    while True:
+        event, values = window.read()
+        if event == gui.WIN_CLOSED:
+            exit()
+        elif event == "Add tenant":
+            gui.sgprint_close()
+            addtenant()
+            break
+        
+    
+
+def addtenant():
 
     layout = [ [gui.Text("Tenant name: ")],
-               [gui.InputText()],
-               [gui.Text("Plot number: ")],
-               [gui.InputText()],
-               [gui.Text("Tenant phone number: ")],
-               [gui.InputText()],
-               [gui.Text("Enter room number: ")],
-               [gui.InputText()],
-               [gui.Text("Rent p/month: ")],
-               [gui.InputText()],
-               [gui.Text("Start date: ")],
-               [gui.InputText()],
-               [gui.Text("End date: ")],
-               [gui.InputText()],
-
-               [gui.Button('Ok')], [gui.Button('Cancel')]
-
-                
-            ]
+                [gui.InputText()],
+                [gui.Text("Plot number: ")],
+                [gui.InputText()],
+                [gui.Text("Tenant phone number: ")],
+                [gui.InputText()],
+                [gui.Text("Enter room number: ")],
+                [gui.InputText()],
+                [gui.Text("Rent p/month: ")],
+                [gui.InputText()],
+                [gui.Text("Start date (DD/MM/YYYY)")],
+                [gui.InputText()],
+                [gui.Text("End date (DD/MM/YYYY)")],
+                [gui.InputText()],
+                [gui.Button('Ok')], [gui.Button('Cancel')]    
+                ]
+    
     window = gui.Window("Hello world", layout)
 
     while True:
         event, values = window.read()
-        if event == gui.WIN_CLOSED or event == "Cancel":
+        if event == gui.WIN_CLOSED or event == "Cancel" or event == "Ok":
             break
-    
+        
     name = values[0]
     plot = values[1]
     phone = values[2]
@@ -37,8 +55,6 @@ def main():
     startdate = values[4]
     enddate = values[5]
 
-    print(f"Name is {values[0]}")
-
     conn = sqlite3.connect("database1")
     cur = conn.cursor()
 
@@ -46,16 +62,7 @@ def main():
     conn.commit()
     cur.execute(f"INSERT INTO tenants VALUES ('{name}', '{plot}', '{phone}', '{room}' , '{rent}', '{startdate}', '{enddate}')")
     conn.commit()
-def databasecreate():
-    conn = None
-    try:
-        conn = sqlite3.connect("database1")
-        print(sqlite3.sqlite_version)
-    except sqlite3.Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close
+
 
 main()
     
